5b5d078a53a7881051875e4b164220c0427795e7
ekannuth-dev <ekannuth@uwaterloo.ca>
2025-04-06 15:25:32 -0400

    Initial Commit

---
+++ Speer-iOS-challenge/Speer_iOS_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Speer_iOS_challengeApp.swift
+//  Speer-iOS-challenge
+//
+//  Created by Ehan kannuthurai on 2025-04-06.
+//
+
+import SwiftUI
+
+@main
+struct Speer_iOS_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
